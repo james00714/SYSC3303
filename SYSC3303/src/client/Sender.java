b71f15e1395893445985e87a2c365a9c5ec4ad2c
jasonli3 <35589512+jasonli3@users.noreply.github.com>
2018-07-14 22:28:04 -0400

    add it2

---
+++ SYSC3303/src/client/Sender.java
@@ -0,0 +1,222 @@
+package client;
+
+import java.io.*;
+import java.net.*;
+
+public class Sender {
+	private Client c;
+	private DatagramSocket sendReceiveSocket;
+	private DatagramPacket receivePacket, sendPacket;
+	private FileHandler fileHandler;
+	private RequestParser RP;
+	private int blockNumber = 0, port;
+
+	public Sender(Client c){
+		this.c = c;
+		RP = new RequestParser();
+		try {
+			sendReceiveSocket = new DatagramSocket();
+		} catch (SocketException se) {   
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public DatagramPacket getReceivePacket() {
+		return this.receivePacket;
+	}
+
+	public DatagramPacket getSendPacket() {
+		return this.sendPacket;
+	}
+
+	public void Receiver () throws IOException{
+		System.out.println("Client: waiting a packet...");
+		byte data[] = new byte[1024];
+		receivePacket = new DatagramPacket(data, data.length);
+
+		try {
+			sendReceiveSocket.receive(receivePacket);
+
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		PrintReceiver(receivePacket);
+		ReceiveHandler(receivePacket);
+
+	}
+
+	public void ReceiveHandler (DatagramPacket receivePacket) throws IOException{
+		RP.parseRequest(receivePacket.getData(), receivePacket.getLength());
+
+		switch (RP.getType()) {
+		case 3:	DATA(receivePacket);
+		break;
+		case 4:	ACK(receivePacket);
+		break;
+		case 5:	ERR();
+		break;
+		}
+	}
+
+	public void DATA (DatagramPacket receivePacket) throws IOException{
+		System.out.println("Received Data packet.");
+		byte [] send = new byte [4];
+		int blockNum = RP.getBlockNum();
+		int length = receivePacket.getLength();
+
+		if (blockNum == blockNumber){
+			fileHandler.writeFile(RP.getFileData());
+			if (length == 516){			
+				send[0] = 0;
+				send[1] = 4;
+				send[2] = (byte)(blockNumber/256);
+				send[3] = (byte)(blockNumber%256);
+				SendPacket (send);
+				blockNumber++;
+				Receiver();
+			}
+			else {
+				fileHandler.close();
+			}
+		}	
+		else {
+			System.out.println("Error, block number don't match");
+		}
+	}
+
+
+	public void ACK (DatagramPacket receivePacket) throws IOException{
+		System.out.println("Received ACK packet.");
+		byte [] send; 
+		int blockNum = RP.getBlockNum();
+
+		if (blockNum == blockNumber){
+			byte[] fileData = fileHandler.readFile();
+			int length = fileData.length;
+			blockNumber++;
+			send = new byte [4+ length];
+			send[0] = 0;
+			send[1] = 3;
+			send[2] = (byte)(blockNumber/256);
+			send[3] = (byte)(blockNumber%256);
+			for (int i = 0; i < fileData.length; i++){
+				send[4+i] = fileData[i];
+			}
+			SendPacket(send);
+
+			if (fileData.length == 512){	
+				Receiver();
+			}	
+			else {
+				fileHandler.close();
+			}
+		}	
+		else{
+			System.out.println("Error");
+		}	
+	}
+
+	public void ERR (){
+		System.out.println("Received error packet.");
+	}
+
+	public void SendPacket(byte [] packet) {
+		//		System.out.println("Client: sending a packet...");
+		try {
+			sendPacket = new DatagramPacket(packet, packet.length,
+					InetAddress.getLocalHost(), port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}	
+		/*
+		try {
+			Thread.sleep(500);
+		}catch(InterruptedException e){}
+		*/
+		//		Send packet
+		try {
+			// displaySend(sendPacket);
+			sendReceiveSocket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		PrintSender(sendPacket);
+	}
+
+	public void  RequestHandler(String request, String fileName) throws IOException{
+		byte [] send = null;
+		byte [] length = fileName.getBytes();
+
+		send = new byte [3 +length.length];
+		send[0] = 0;
+		if(request.equals("1")) {
+			send[1] = 1;
+			fileHandler = new FileHandler();
+			fileHandler.prepareWrite(fileName);
+			blockNumber = 1;
+			System.out.println("Read request generated.");
+		}else {
+			send[1] = 2;
+			fileHandler = new FileHandler();
+			fileHandler.readFile(fileName);
+			blockNumber = 0;
+			System.out.println("Write request generated.");
+		}
+
+		for (int i = 0; i < length.length; i++){
+			send[2+i] = length[i];
+		}
+
+		send[2+length.length] = 0;
+
+		System.out.println("printing out byte array");
+		for (int i = 0; i < send.length; i++) {
+			System.out.println(send[i]);
+		}
+
+		SendPacket (send);
+
+	}
+
+
+	public void PrintReceiver (DatagramPacket receivePacket){
+		Verbose v = new Verbose();
+		Quiet q = new Quiet();
+
+		if (c.getFig().equals("1")) {
+			v.PrintReceiverV(receivePacket);
+		}
+		else {
+			q.PrintReceiverQ(receivePacket);
+		}	
+	}
+
+
+	public void PrintSender (DatagramPacket sendPacket) {
+		Verbose v = new Verbose();
+		Quiet q = new Quiet();
+
+		if (c.getFig().equals("1")) {
+			v.PrintSender(sendPacket);
+		}
+		else {
+			q.PrintSenderQ(sendPacket);
+		}
+	}
+
+	public void Close (){
+		sendReceiveSocket.close();
+	}
+
+	public void start (Client c, Sender s, int portNum) throws IOException{
+		System.out.println("Normal mode selected");
+		this.port = portNum;
+		s.RequestHandler(c.getRequest(), c.getFileName());
+	}
+
+}
---
+++ SYSC3303/src/server/FileHandler.java
@@ -0,0 +1,134 @@
+/*
+ * FileHander class to handle requests that need file operation
+ * */
+
+package server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileHandler {
+
+	private FileInputStream fs;
+	private FileOutputStream os;
+	private File fileToWrite;
+	private byte[] fileBuffer;
+
+	public FileHandler() {}
+
+	/*
+	 * Method to handle read request
+	 * In: file path or name
+	 * */
+	public byte[] readFile(String file) throws IOException {
+
+		if(!file.contains("\\")) {
+			file = "src\\server\\files\\" + file;
+		}	
+		
+		//	Try loading the file
+		int count;
+		fs = new FileInputStream(file);
+		fileBuffer = new byte[512];	
+		if ((count = fs.read(fileBuffer)) != -1){
+			
+			//	If reached the end of file, size will be reduced
+			if(count < fileBuffer.length) {
+				byte[] tempBuffer = new byte[count];
+				for(int i = 0; i < count; i++) {
+					tempBuffer[i] = fileBuffer[i];
+				}
+				fileBuffer = tempBuffer;
+			}
+		}
+
+		// return data loaded
+		return fileBuffer;
+	}
+
+	/*
+	 * Method to read the rest of file
+	 * In: file path or name
+	 * */
+	public byte[] readFile() {
+
+		int count;
+		fileBuffer = new byte[512];
+
+		try {
+			if ((count = fs.read(fileBuffer)) != -1){
+				//	If reached the end of file, size will be reduced
+				if(count < fileBuffer.length) {
+					byte[] tempBuffer = new byte[count];
+					for(int i = 0; i < count; i++) {
+						tempBuffer[i] = fileBuffer[i];
+					}
+					fileBuffer = tempBuffer;
+				}
+			}		
+		}catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+		// return data loaded
+		return fileBuffer;
+	}
+
+	/*
+	 * Method to prepare work before write a file
+	 * In: file path or name
+	 * */
+	public void prepareWrite(String file) {
+
+		System.out.println("Preparing writing file...");
+		
+		if(file.contains("\\")) {
+			int index = 0;
+			for(int i = 0; i < file.length(); i++){
+				if(file.charAt(i) == '\\'){
+					index = i;
+				}
+			}
+			file = file.substring(index, file.length());
+		}
+		file = "src\\server\\files\\" + file;
+		try{
+			fileToWrite = new File(file);
+			// create if does not exists
+			if (!fileToWrite.exists()) {
+				fileToWrite.createNewFile();
+			}
+			// set output stream to write file
+			os = new FileOutputStream(fileToWrite);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Method to close stream
+	 * In: file data
+	 * */
+	public void close() throws IOException{
+		if(fs != null) {
+			fs.close();
+		}
+		if(os != null){
+			os.close();
+		}
+	}
+
+	/*
+	 * Method to prepare write data to a file
+	 * In: file data
+	 * */
+	public void writeFile(byte[] fileData) throws IOException {
+		
+		System.out.println("Writing file...");
+		
+		os.write(fileData);
+		os.flush();
+	}
+}
