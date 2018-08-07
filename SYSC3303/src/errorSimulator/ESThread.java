2c5013c9dc82b1ed0e83421bdc56b5cb71af23d6
james00714 <32552040+james00714@users.noreply.github.com>
2018-08-07 19:43:37 -0400

    update-it5es

--- SYSC3303/src/errorSimulator/ESThread.java
+++ SYSC3303/src/errorSimulator/ESThread.java
@@ -221,14 +221,10 @@ public class ESThread extends Thread{
 					transferPacket(modifyFilename(receivedPacket, errorFilename),receiveSendSocket); 
 					break;
 				case 4: 
-					System.out.println("Modify Packet size for target" + parsePacketName(rp.getType()) + " Packet ...");
-					transferPacket(modifyPacketSize(receivedPacket, errorPacketSize),receiveSendSocket); 
-					break;
-				case 5: 
 					System.out.println("Modify Packet format for target" + parsePacketName(rp.getType()) + " Packet ...");
 					transferPacket(modifyPacketFormat(receivedPacket, errorPacketFormat),receiveSendSocket); 
 					break;
-				case 6: 
+				case 5: 
 					System.out.println("Making Error Code 5 for target" + parsePacketName(rp.getType()) + " Packet ...");
 					transferPacket(modifyOpcode(receivedPacket, 3),receiveSendSocket); 
 					break;
